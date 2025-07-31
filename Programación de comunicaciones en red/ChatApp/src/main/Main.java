8083b41f8f42796e1353cbddbdf4905c91ef94d6
Kevin Gómez Valderas <kgv1722002@hotmail.com>
2025-07-31 19:21:02 +0200

    falta solucionar delete servidor

--- Programación de comunicaciones en red/ChatApp/src/main/Main.java
+++ Programación de comunicaciones en red/ChatApp/src/main/Main.java
@@ -40,7 +40,33 @@ public class Main {
      */
     public static void main(String args[]) {
 
-        ConsoleMode.runCLI(); // Ejecutar en modo CLI
+        if (GraphicsEnvironment.isHeadless()) {
+
+            ConsoleMode.runCLI(); // Ejecutar en modo CLI
+
+        } else {
+
+            // Ejecutar en modo GUI
+            try {
+
+                for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+
+                    if ("Nimbus".equals(info.getName())) {
+                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
+
+                }
+
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+                java.util.logging.Logger.getLogger(ChatApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            }
+
+            java.awt.EventQueue.invokeLater(() -> {
+                new ChatApp().setVisible(true);
+            });
+
+        }
 
     }
 
